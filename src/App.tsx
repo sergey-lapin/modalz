75dcee4e79f0e8e2ba1206980d47760b82962fe7
Sergey Lapin <lapanoid@gmail.com>
2020-10-03 13:08:29 +0300

    fix button

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const Modal = ({ children, x, y, onClose, onFocus }: {
 type RefType = ((instance: HTMLButtonElement | null) => void) | React.MutableRefObject<HTMLButtonElement | null> | null
 
 const Button = React.forwardRef(({ children, onClick }: { children: any, onClick: () => void }, ref: RefType) => {
-  const longPress = useLongPress(onClick, 100);
+  const longPress = useLongPress(onClick, 150);
   return (<button className="button" {...longPress} ref={ref}>
     {children}
   </button>)
