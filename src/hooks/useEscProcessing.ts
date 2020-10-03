97e7315d7808f72f219d3c28b027ac3155200015
Sergey Lapin <lapanoid@gmail.com>
2020-10-03 23:16:37 +0300

    fix small bug in useEscProcessing

--- src/hooks/useEscProcessing.ts
+++ src/hooks/useEscProcessing.ts
@@ -1,6 +1,19 @@
 import * as React from "react";
 let listeners: ((e: KeyboardEvent) => void)[] = [];
 
+const addCallback = (callback: (e: KeyboardEvent) => void) => {
+    for (let listener of listeners) {
+        document.removeEventListener('keydown', listener)
+    }
+
+    document.addEventListener('keydown', callback);
+
+    for (let i = listeners.length - 1; i >= 0; i--) {
+        document.addEventListener('keydown', listeners[i]);
+    }
+    listeners.push(callback);
+}
+
 export const useEscProcessing = ({ onEsc, onEscCloseAll }: { onEsc: Function, onEscCloseAll?: Function }) => {
     React.useLayoutEffect(() => {
         let callback = (e: KeyboardEvent) => {
@@ -20,18 +33,10 @@ export const useEscProcessing = ({ onEsc, onEscCloseAll }: { onEsc: Function, on
             }
         }
 
-        for (let listener of listeners) {
-            document.removeEventListener('keydown', listener)
-        }
-
-        document.addEventListener('keydown', callback);
-
-        for (let i = listeners.length - 1; i >= 0; i--) {
-            document.addEventListener('keydown', listeners[i]);
-        }
-        listeners.push(callback);
+        addCallback(callback)
 
         return () => {
+            listeners = []
             document.removeEventListener('keydown', callback)
         }
     }, [onEsc, onEscCloseAll])
